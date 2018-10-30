8d35c1ad6b2327d69a85949232ec1f9f1467469d
Adilkhan <adilkhan.kozhakhmetov@gmail.com>
2018-10-30 11:54:11 +0600

    Lab 4

---
+++ LAB4/Problem5/src/MinMax.java
@@ -0,0 +1,19 @@
+public class MinMax {
+    static class Pair{
+        int min, max;
+        Pair(int min, int max) {
+            this.min = min;
+            this.max = max;
+        }
+    }
+
+    static Pair minmax(int values[]) {
+        int curmx = values[0];
+        int curmn = values[0];
+        for(int it : values) {
+            curmx = Math.max(curmx, it);
+            curmn = Math.min(curmn, it);
+        }
+        return new Pair(curmn, curmx);
+    }
+}
